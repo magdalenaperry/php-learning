9da0585bc1c238488372b295b25fde20d190bb7d
Magdalena Perry <magperry16@gmail.com>
2022-08-05 08:54:56 -0400

    including files

---
+++ forloop.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <meta>
+    <title>Get vs Post Forms</title>
+  </head>
+  <body>
+
+
+    <?php 
+      // $index = 1; 
+      // while($index <=5){
+      //   echo "$index <br>";
+      //   $index++;
+      // }
+
+      $pets = array("dog", "cat", "pig");
+      for( $i = 0; $i < count($pets); $i++){
+        echo "$pets[$i] <br>";
+      }
+
+
+    ?>
+
+  </body>    
+</html>
---
+++ includes.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Magdalena's Site</title>
+</head>
+<body>
+  <?php include "header.html" ?>
+
+
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque consectetur incidunt error impedit tenetur minima odit, repellat aliquam suscipit tempore. Aspernatur voluptas omnis numquam labore voluptates, nulla eius fuga aut? <p>
+
+  <?php include "footer.html" ?>
+</body>
+</html>
